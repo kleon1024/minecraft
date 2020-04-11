9a3cbebc2e04f5ad51a27d9bdb304a948f38ff74
Ding Li <1995dingli@gmail.com>
2020-04-11 16:56:15 +0800

    modify

--- start.sh
+++ start.sh
@@ -1,17 +1,17 @@
-apt update
+sudo apt update
 
-apt install -y docker-compose
+sudo apt install -y docker-compose
 
-cp -rf daemon.json /etc/docker
-systemctl daemon-reload
-systemctl restart docker
+sudo cp -rf daemon.json /etc/docker
+sudo systemctl daemon-reload
+sudo systemctl restart docker
 
-ssh-keygen -q
+ssh-keygen -q -t rsa
 ssh-copy-id ubuntu@172.17.0.2
 
 rsync -avP ubuntu@172.17.0.2:~/minecraft ~
 
 cd minecraft
-docker-compose -f mc-compose.yaml up -d
+sudo docker-compose -f mc-compose.yaml up -d
 
 
