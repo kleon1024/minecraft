34e78bd92550d4cf54ae0fecf02965dd3c649ca2
Ding Li <1995dingli@gmail.com>
2020-04-11 16:51:00 +0800

    modify scripts

--- env.sh
+++
@@ -1,5 +0,0 @@
-apt update
-apt install -y docker-ce docker-compose
-echo daemon.json >> /etc/docker/daemon.json
-systemctl daemon-reload
-systemctl restart docker
--- start.sh
+++ start.sh
@@ -1,11 +1,12 @@
 apt update
 
-apt install -y docker-ce docker-compose
+apt install -y docker-compose
 
 cp -rf daemon.json /etc/docker
 systemctl daemon-reload
 systemctl restart docker
 
+ssh-keygen -q
 ssh-copy-id ubuntu@172.17.0.2
 
 rsync -avP ubuntu@172.17.0.2:~/minecraft ~
